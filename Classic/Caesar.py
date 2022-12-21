e897368f1e058dede406c01eb59bebcaa5faf5e0
is-not-avaliable <108990590+is-not-avaliable@users.noreply.github.com>
2022-12-20 23:38:54 -0300

    Update Caesar.py

--- Classic/Caesar.py
+++ Classic/Caesar.py
@@ -1,36 +1,38 @@
+#!usr/bin/env python3
+# -*- coding: ascii -*-
 import sys
 
 def encrypt(realText, step):
-    result = ""  
-     
-    for i in range(len(realText)):  
-        char = realText[i]  
-        
-        if (char.isupper()):  
-            result += chr((ord(char) - 1 + step - 64) % 26 + 65)  
-          
-        else:    
-            result += chr((ord(char) - 1 + step - 96) % 26 + 97)  
-    return result  
+    result = ""
+
+    for i in range(len(realText)):
+        char = realText[i]
+
+        if (char.isupper()):
+            result += chr((ord(char) - 1 + step - 64) % 26 + 65)
+
+        else:
+            result += chr((ord(char) - 1 + step - 96) % 26 + 97)
+    return result
 
 def decrypt(realText, step):
-    result = ""  
+    result = ""
 
-    for i in range(len(realText)):  
-        char = realText[i]  
-        
+    for i in range(len(realText)):
+        char = realText[i]
 
-        if (char.isupper()):  
-            result += chr((ord(char) - 1 - step - 64) % 26 + 65)  
-          
-        else:    
-            result += chr((ord(char) - 1 - step - 96) % 26 + 97)  
-    return result  
+
+        if (char.isupper()):
+            result += chr((ord(char) - 1 - step - 64) % 26 + 65)
+
+        else:
+            result += chr((ord(char) - 1 - step - 96) % 26 + 97)
+    return result
 
 if __name__=="__main__":
     if len(sys.argv) == 1:
         print("How to use: python3 caesar.py [-E, -D] string numeralkey")
-        
+
     else:
         if sys.argv[1] == "-E":
             print(encrypt(sys.argv[2], int(sys.argv[3])))
